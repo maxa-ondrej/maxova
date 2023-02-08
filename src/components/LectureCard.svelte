4f2bad15346b1350355dfe7c07b50a1d3a6fd9d3
Ondřej Maxa <47477160+maxa-ondrej@users.noreply.github.com>
2023-02-08 18:29:42 +0000

    💄 add responsibility

--- src/components/LectureCard.svelte
+++ src/components/LectureCard.svelte
@@ -2,16 +2,17 @@
 	import { _ } from 'svelte-i18n';
 	import { route } from '$lib/url';
 
-	export let type;
-	export let color;
-	export let id;
-	export let day;
-	export let time;
-	export let room;
+	export let type: string;
+	export let color: string | undefined = undefined;
+	export let id: string;
+	export let day: string;
+	export let time: string;
+	export let room: string;
+	export let span: number;
 	let uuid = $_(`days.short.${day}`) + '_' + time.replace(':', '_');
 </script>
 
-<div class="card" style="--color: {color}">
+<div class="card" style="--color: {color}; --span: {span}">
 	<h4><i>{$_('lectures.practice')}</i></h4>
 	<h2>{$_(`lectures.${type}`)}</h2>
 	<h4><i>{id}</i></h4>
@@ -24,21 +25,39 @@
 </div>
 
 <style lang="scss">
-	.card {
-		box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
-		border-radius: 5px;
-		background-color: var(--surface);
-		margin: 20px;
-		padding: 20px;
-		text-align: center;
-		float: left;
+.card {
+	box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+	border-radius: 5px;
+	background-color: var(--surface);
+	margin: 20px;
+	padding: 20px;
+	text-align: center;
+	grid-column: span var(--span);
 
-		.title {
-			font-size: 25px;
-		}
-
-		p {
-			margin-bottom: 15px;
-		}
+	.title {
+		font-size: 25px;
 	}
+
+	p {
+		margin-bottom: 15px;
+	}
+}
+
+@media only screen and (max-width: 1200px) {
+	.card {
+		grid-column: span 4;
+	}
+}
+
+@media only screen and (max-width: 768px) {
+	.card {
+		grid-column: span 6;
+	}
+}
+
+@media only screen and (max-width: 480px) {
+	.card {
+		grid-column: span 12;
+	}
+}
 </style>
--- src/routes/teaching/+page.svelte
+++ src/routes/teaching/+page.svelte
@@ -4,26 +4,34 @@
 	import MoodleButton from '../../components/MoodleButton.svelte';
 </script>
 
-<LectureCard type="la2" id="NMAI058" day="monday" time="12:20" room="N7">
-	<MoodleButton id="14561" slot="button" />
-</LectureCard>
-<LectureCard type="la2" id="NMAI058" day="monday" time="15:40" room="N7">
-	<MoodleButton id="14561" slot="button" />
-</LectureCard>
-<LectureCard type="la2" id="NMAI058" day="wednesday" time="15:40" room="N6">
-	<MoodleButton id="14561" slot="button" />
-</LectureCard>
+<div class="lectures">
+	<LectureCard span={4} type="la2" id="NMAI058" day="monday" time="12:20" room="N7">
+		<MoodleButton id="14561" slot="button" />
+	</LectureCard>
+	<LectureCard span={4} type="la2" id="NMAI058" day="monday" time="15:40" room="N7">
+		<MoodleButton id="14561" slot="button" />
+	</LectureCard>
+	<LectureCard span={4} type="la2" id="NMAI058" day="wednesday" time="15:40" room="N6">
+		<MoodleButton id="14561" slot="button" />
+	</LectureCard>
 
-<LectureCard type="ma1" id="NMAI054" day="tuesday" time="12:20" room="N4">
-	<MoodleButton id="14562" slot="button" />
-</LectureCard>
-<LectureCard type="ma1" id="NMAI054" day="tuesday" time="14:00" room="N5">
-	<MoodleButton id="14562" slot="button" />
-</LectureCard>
-<LectureCard type="ma1" id="NMAI054" day="wednesday" time="12:20" room="N6">
-	<MoodleButton id="14562" slot="button" />
-</LectureCard>
+	<LectureCard span={3} type="ma1" id="NMAI054" day="tuesday" time="12:20" room="N4">
+		<MoodleButton id="14562" slot="button" />
+	</LectureCard>
+	<LectureCard span={3} type="ma1" id="NMAI054" day="tuesday" time="14:00" room="N5">
+		<MoodleButton id="14562" slot="button" />
+	</LectureCard>
+	<LectureCard span={3} type="ma1" id="NMAI054" day="wednesday" time="12:20" room="N6">
+		<MoodleButton id="14562" slot="button" />
+	</LectureCard>
+	<LectureCard span={3} type="ma1" id="NMAI054" day="wednesday" time="14:00" room="N6">
+		<MoodleButton id="14562" slot="button" />
+	</LectureCard>
+</div>
 
-<LectureCard type="ma1" id="NMAI054" day="wednesday" time="14:00" room="N6">
-	<MoodleButton id="14562" slot="button" />
-</LectureCard>
+<style lang="scss">
+div.lectures {
+	display: grid;
+	grid-template-columns: repeat(12, 1fr);
+}
+</style>
